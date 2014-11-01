101aa9bf62f6efab2f5d54e7044b9eb59e2add2e
Courteney Ervin <courteney.ervin@gmail.com>
2014-11-01 15:06:25 -0400

    Add GitHub url validator methods to organizations controller.

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,6 +1,8 @@
-class OrganizationsController < ApplicationController
+require 'net/http'
+require 'open-uri'
 
-	def new
+class OrganizationsController < ApplicationController
+  def new
 		@organization = Organization.new
 		@project = @organization.projects.build(params[:projects])
 	end
@@ -19,11 +21,39 @@ class OrganizationsController < ApplicationController
   def index
     @featured_orgs = Organization.featured
   end
-  
+
   def show
     @organization = Organization.find(params[:id])
     @jobs = @organization.jobs.active
     @projects = @organization.projects.featured.includes :technologies, :causes
     @sponsorships = @organization.sponsorships
+  end
+
+  # Github URL validation
+  GITHUB_URL_REGEX = /\A(http:\/\/|https:\/\/|www.)*github.com\/(\S+)\/(\S+)(.git)*\z/
+
+  def github_url?(url)
+    url =~ GITHUB_URL_REGEX
+  end
+
+  def valid_url?(url)
+    response = Net::HTTP.get_response(URI(url))
+    response.code == 200 || 301
+  end
+
+  def parse_github_url(url)
+    url =~ GITHUB_URL_REGEX
+    github_org = $2
+    github_repo = $3
+
+    {github_org: github_org, github_repo: github_repo}
+  end
+
+  def get_github_details(url)
+    if valid_url?(url) && github_url?(url)
+      parse_github_url(url)
+    else
+      raise "The submitted url is not valid."
+    end
   end
 end
