a33948757c98dd50b27b835cfa2c9101d04297d1
Courteney Ervin <courteney.ervin@gmail.com>
2014-11-02 07:24:23 -0500

    Add methods to assist github link validation to lib

---
+++ lib/github_link_validator.rb
@@ -0,0 +1,31 @@
+require 'net/http'
+require 'open-uri'
+
+module GithubLinkValidator
+  GITHUB_URL_REGEX = /\A(http:\/\/|https:\/\/|www.)*github.com\/(\S+)\/(\S+)(.git)*\z/
+
+  def github_url?(url)
+    url =~ GITHUB_URL_REGEX
+  end
+
+  def valid_url?(url)
+    response = Net::HTTP.get_response(URI(url))
+    response.code == 200 || 301
+  end
+
+  def parse_github_url(url)
+    url =~ GITHUB_URL_REGEX
+    github_org = $2
+    github_repo = $3
+
+    {github_org: github_org, github_repo: github_repo}
+  end
+
+  def get_github_details(url)
+    if valid_url?(url) && github_url?(url)
+      parse_github_url(url)
+    else
+      raise "The submitted url is not valid."
+    end
+  end
+end
