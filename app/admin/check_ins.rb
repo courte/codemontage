01babd2e9306df3279cbea1ec1de2a448f914fc6
Courteney Ervin <courteney.ervin@gmail.com>
2015-01-08 01:32:02 -0500

    Change event registrations to check ins in the admin area

---
+++ app/admin/check_ins.rb
@@ -0,0 +1,21 @@
+ActiveAdmin.register CheckIn do
+  menu parent: 'Event'
+
+  index do
+    column 'Event', sortable: :event_name do |check_in|
+      link_to check_in.event.name, admin_event_path(check_in.event)
+    end
+    column 'User', sortable: :user_name do |check_in|
+      link_to check_in.user.name, admin_user_path(check_in.user)
+    end
+    column :created_at
+    column :updated_at
+
+    default_actions
+  end
+
+  filter :event_id
+  filter :user_id, label: 'User ID', as: :numeric
+  filter :created_at
+  filter :updated_at
+end
