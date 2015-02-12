5b8ad665cf868f9b719adb307c2d8961f780ce4d
Courteney Ervin <courteney.ervin@gmail.com>
2015-02-12 10:00:17 -0500

    Adjust syntax according to HoundCI recommendations

--- db/migrate/20150211210612_create_user_engaged_projects.rb
+++ db/migrate/20150211210612_create_user_engaged_projects.rb
@@ -1,10 +1,10 @@
 class CreateUserEngagedProjects < ActiveRecord::Migration
   def change
     create_table :user_engaged_projects do |t|
-      t.string      :project_github_url, null: false
-      t.string      :engagement_type,    null: false
-      t.datetime    :github_created_at,  null: false
-      t.references  :user,               null: false
+      t.string :project_github_url,     null: false
+      t.string :engagement_type,        null: false
+      t.datetime :github_created_at,    null: false
+      t.references :user,               null: false
       t.timestamps
     end
 
